58c6a7185259e1ef5837900cdb3da4b4e903d670
BALU SODADASU <balusodadasu@gmail.com>
2026-09-06 11:36:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Change the Case based on First Character/change-the-case-based-on-first-character.java
@@ -0,0 +1,9 @@
+class Solution {
+    String modify(String s) {
+        if(Character.isUpperCase(s.charAt(0)))
+        {
+            return s.toUpperCase();
+        }
+        return s.toLowerCase();
+    }
+}
