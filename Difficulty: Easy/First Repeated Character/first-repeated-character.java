ab6426e962169b8b17c7988f9ecb715c633399b2
BALU SODADASU <balusodadasu@gmail.com>
2026-09-07 20:07:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeated Character/first-repeated-character.java
@@ -0,0 +1,17 @@
+class Solution {
+	String firstRepChar(String s) {
+		// code here
+		HashSet<Character> set = new HashSet<>();
+		
+		for (char ch : s.toCharArray()) {
+			
+			if (set.contains(ch)) {
+				return String.valueOf(ch);
+			}
+			
+			set.add(ch);
+		}
+		
+		return "-1";
+	}
+}
