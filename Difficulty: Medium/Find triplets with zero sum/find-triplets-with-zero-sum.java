5801bebea1dd8181952a514efca315af3d7539eb
BALU SODADASU <balusodadasu@gmail.com>
2026-09-20 20:19:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find triplets with zero sum/find-triplets-with-zero-sum.java
@@ -0,0 +1,31 @@
+class Solution {
+	public boolean findTriplets(int[] arr) {
+		// code here.
+		Arrays.sort(arr);
+		
+		int n = arr.length;
+		
+		for (int i = 0; i < n - 2; i++) {
+			
+			int left = i + 1;
+			int right = n - 1;
+			
+			while (left < right) {
+				
+				int sum = arr[i] + arr[left] + arr[right];
+				
+				if (sum == 0) {
+					return true;
+				}
+				else if (sum < 0) {
+					left++;
+				}
+				else {
+					right--;
+				}
+			}
+		}
+		
+		return false;
+	}
+}
